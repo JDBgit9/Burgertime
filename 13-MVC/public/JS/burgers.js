9a167bc7347142fafef32773b3a2f7fa93dd8c61
JDBgit9 <info@jamesdbeeks.com>
2020-10-19 16:51:29 -0400

    added all of the new burger names, completely changed the syntax of the code to sync up still working have to get to css now

--- 13-MVC/public/JS/burgers.js
+++ 13-MVC/public/JS/burgers.js
@@ -1,61 +1,51 @@
-// Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
-    $(".create-burger").on("click", function(event) {
-      var id = $(this).data("id");
-      var newSleep = $(this).data("devoured");
-  
-      var newSleepState = {
-        sleepy: newSleep
-      };
-  
-      // Send the PUT request.
-      $.ajax("/api/burger/" + id, {
-        type: "PUT",
-        data: 
-      }).then(
-        function() {
-          console.log("changed sleep to", newSleep);
-          // Reload the page to get the updated list
-          location.reload();
-        }
-      );
-    });
-  
-    $(".create-form").on("submit", function(event) {
-      // Make sure to preventDefault on a submit event.
-      event.preventDefault();
-  
-      var newBurger = {
-        name: $("#ca").val().trim(),
-        sleepy: $("[name=sleepy]:checked").val().trim()
-      };
-  
-      // Send the POST request.
-      $.ajax("/api/burgers", {
-        type: "POST",
-        data: newBurger
-      }).then(
-        function() {
-          console.log("created new burger");
-          // Reload the page to get the updated list
-          location.reload();
-        }
-      );
-    });
-  
-    $(".delete-burgers").on("click", function(event) {
-      var id = $(this).data("id");
-  
-      // Send the DELETE request.
-      $.ajax("/api/burgers/" + id, {
-        type: "DELETE"
-      }).then(
-        function() {
-          console.log("deleted burgers", id);
-          // Reload the page to get the updated list
-          location.reload();
-        }
-      );
+  $(".create-form").on("submit", function(event) {
+    event.preventDefault();
+
+    var newBurger = {
+      burger_name: $("#newburger")
+        .val()
+        .trim(),
+      devoured: 0
+    };
+
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger
+    }).then(function() {
+      console.log("Added new burger");
+      location.reload();
     });
   });
+
+  $(".eatburger").on("click", function(event) {
+    event.preventDefault();
+
+    var id = $(this).data("id");
+    var devouredState = {
+      devoured: 1
+    };
+
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: devouredState
+    }).then(function() {
+      console.log("Burger devoured");
+      location.reload();
+    });
+  });
+
+  $(".trashburger").on("click", function(event) {
+    event.preventDefault();
+
+    var id = $(this).data("id");
+
+    // Send the DELETE request.
+    $.ajax({
+      type: "DELETE",
+      url: "/api/burgers/" + id
+    }).then(location.reload());
+  });
+});
+
   
