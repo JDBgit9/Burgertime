2cc29a8c57224cbdce5f203753170d03380cc90c
JDBgit9 <info@jamesdbeeks.com>
2020-10-18 22:06:20 -0400

    i finally got it to work mad most of the name changes

--- 13-MVC/controllers/burgers_controller.js
+++ 13-MVC/controllers/burgers_controller.js
@@ -3,7 +3,7 @@ var express = require("express");
 var router = express.Router();
 
 // Import the model (burge.js) to use its database functions.
-var burge = require("../models/burger.js");
+var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
@@ -18,7 +18,7 @@ router.get("/", function(req, res) {
 
 router.post("/api/burger", function(req, res) {
   burger.create([
-    "name", "sleepy"
+    "burger_name", "devoured"
   ], [
     req.body.name, req.body.sleepy
   ], function(result) {
@@ -33,7 +33,7 @@ router.put("/api/burger/:id", function(req, res) {
   console.log("condition", condition);
 
   burger.update({
-    sleepy: req.body.sleepy
+    devoured: req.body.devoured
   }, condition, function(result) {
     if (result.changedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
--- 13-MVC/public/JS/burgers.js
+++ 13-MVC/public/JS/burgers.js
@@ -1,6 +1,6 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
-    $(".change-sleep").on("click", function(event) {
+    $(".change-burger").on("click", function(event) {
       var id = $(this).data("id");
       var newSleep = $(this).data("newsleep");
   
@@ -9,7 +9,7 @@ $(function() {
       };
   
       // Send the PUT request.
-      $.ajax("/api/burger/" + id, {
+      $.ajax("/api/burgers/" + id, {
         type: "PUT",
         data: newSleepState
       }).then(
@@ -25,33 +25,33 @@ $(function() {
       // Make sure to preventDefault on a submit event.
       event.preventDefault();
   
-      var newCat = {
+      var newBurger = {
         name: $("#ca").val().trim(),
         sleepy: $("[name=sleepy]:checked").val().trim()
       };
   
       // Send the POST request.
-      $.ajax("/api/burger", {
+      $.ajax("/api/burgers", {
         type: "POST",
-        data: newCat
+        data: newBurger
       }).then(
         function() {
-          console.log("created new cat");
+          console.log("created new burger");
           // Reload the page to get the updated list
           location.reload();
         }
       );
     });
   
-    $(".delete-cat").on("click", function(event) {
+    $(".delete-burgers").on("click", function(event) {
       var id = $(this).data("id");
   
       // Send the DELETE request.
-      $.ajax("/api/burger/" + id, {
+      $.ajax("/api/burgers/" + id, {
         type: "DELETE"
       }).then(
         function() {
-          console.log("deleted cat", id);
+          console.log("deleted burgers", id);
           // Reload the page to get the updated list
           location.reload();
         }
